c19025deef159b67d339e64465c3c3a184d62613
trufflesmld <trufflesmld@gmail.com>
2021-11-29 23:02:13 +0300

    create login page

---
+++ src/views/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="logCard">
+    <form>
+      <div class="inputForm">
+        <span class="label">Cabinet-app</span>
+        <div class="inputContainer">
+          <input id="email" type="text" placeholder="  Email" />
+          <!-- <small class="helperText invalid">Email</small> -->
+        </div>
+        <div class="inputContainer">
+          <input id="password" type="password" placeholder="  Password" />
+          <!-- <small class="helperText invalid">Password</small> -->
+        </div>
+      </div>
+      <div class="submitForm">
+        <div class="submitBtn">
+          <button type="submit">
+            Log in
+          </button>
+        </div>
+
+        <p>
+          Don't have an account?
+          <router-link to="/register">Register now</router-link>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.logCard {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  box-shadow: 5px 15px 50px 5px black;
+}
+form {
+  width: 100%;
+  height: 100%;
+}
+.inputForm {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 70%;
+  align-items: center;
+}
+.label {
+  height: 30%;
+  font-size: 300%;
+}
+.inputContainer {
+  width: 100%;
+  height: 35%;
+}
+input {
+  box-sizing: border-box;
+  border: none;
+  border-top: 1px solid rgb(235, 236, 238);
+  border-bottom: 1px solid rgb(235, 236, 238);
+  width: 100%;
+  height: 50%;
+  outline: none;
+}
+.helperText {
+  font-size: 1em;
+}
+.submitForm {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 30%;
+  align-items: center;
+}
+.submitBtn {
+  width: 100%;
+  height: 50%;
+}
+button {
+  border: none;
+  width: 100%;
+  height: 80%;
+  font-size: 1.5em;
+  letter-spacing: 0.1em;
+  font-family: "Teko", sans-serif;
+  background-color: rgb(231,226,232);
+}
+button:hover {
+  background-color: rgb(255, 245, 0);
+  cursor: pointer;
+  transition: 0.4s;
+}
+button:not(:hover) {
+  background-color: rgb(231,226,232);
+  cursor: pointer;
+  transition: 0.4s;
+}
+p {
+  font-size: 1.1em;
+}
+</style>
