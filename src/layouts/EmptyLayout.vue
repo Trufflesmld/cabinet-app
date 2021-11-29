d43ba3dec4d61fa3fde3b8d2760de01ff619a0e6
trufflesmld <trufflesmld@gmail.com>
2021-11-29 20:44:08 +0300

    create emptylayout

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="emptyLayout">
+    <div class="layout">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.emptyLayout {
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(206, 206, 206, 0.527);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.layout {
+    height: 40%;
+    width: 40%;
+}
+
+</style>
