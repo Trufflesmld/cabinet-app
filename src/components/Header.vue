bd2fe5e44ce45e317b1ebf19e6e0158c5f47a84b
trufflesmld <trufflesmld@gmail.com>
2021-11-30 17:01:24 +0300

    upd btn-link for user-profile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,7 @@
   <div class="header">
     <h1 class="logo">Cabinet-app</h1>
     <div class="profile">
-      <router-link to="/UserProfile"/>
-      <h3>User Profile</h3>
+      <router-link to="/UserProfile"><span>User Profile</span></router-link>
     </div>
   </div>
 </template>
@@ -15,7 +14,7 @@
   width: 100vw;
   /* background-color: rgb(255,245,0); */
   background: url("../assets/HeaderBackGround.png");
-  box-shadow: 0 0 20px rgb(235,236,238);
+  box-shadow: 0 0 20px rgb(235, 236, 238);
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -30,12 +29,14 @@ h1 {
   display: flex;
   height: 100%;
 }
-a{
+a {
   height: 100%;
-}
-h3 {
   margin: auto;
   font-size: 3vh;
+  text-decoration: none;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
 </style>
