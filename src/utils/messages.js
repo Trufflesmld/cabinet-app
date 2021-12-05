a4cc2e56b9bce1d46557ff8fc5e5f2c23b05937c
trufflesmld <trufflesmld@gmail.com>
2021-12-05 21:01:52 +0300

    add messages

---
+++ src/utils/messages.js
@@ -0,0 +1,13 @@
+const messages = {
+  "Firebase: Error (auth/email-already-in-use).":
+    "Account with this email already exists",
+  "Firebase: Error (auth/wrong-password).": "Wrong password",
+  "Firebase: Error (auth/user-not-found).":
+    "User with this email does not exist",
+  "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).":
+    "Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).",
+  login: "You are logged in",
+  logout: "You are logged out",
+};
+
+export default messages;
