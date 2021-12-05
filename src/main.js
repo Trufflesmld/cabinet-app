ec78ead4482be4325be2ef36daaa2b3154f35183
trufflesmld <trufflesmld@gmail.com>
2021-12-05 15:22:33 +0300

    add firebase npm

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+initializeApp({
+  apiKey: "AIzaSyDTelvd1eb9J2RYhW57ScUnzkkmP_pOtnw",
+  authDomain: "stp-practice.firebaseapp.com",
+  projectId: "stp-practice",
+  storageBucket: "stp-practice.appspot.com",
+  messagingSenderId: "1032767816951",
+  appId: "1:1032767816951:web:67fc4d10af0afef461bff2",
+  measurementId: "G-NL8MCCWY50",
+});
+
+let application;
+
+const auth = getAuth();
+onAuthStateChanged(auth, () => {
+  if (!application) {
+    application = createApp(App)
+      .use(store)
+      .use(router)
+      .mount("#app");
+  }
+});
