8281d7ab3b26db0b227b568e59b524d0cf4a2dd6
trufflesmld <trufflesmld@gmail.com>
2021-12-06 17:54:04 +0300

    add inf fot table of rates

--- src/components/home-page/HomeCurrency.vue
+++ src/components/home-page/HomeCurrency.vue
@@ -9,30 +9,24 @@
           <tr>
             <th>Currency</th>
             <th>Rate</th>
-            <th>Date</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
+          <tr v-for="cur in currencies" :key="cur.Cur_ID">
+            <td>{{cur.Cur_Abbreviation}}</td>
+            <td>{{cur.Cur_OfficialRate}}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+    props: ['currencies'],
+}
+</script>
 
 <style scoped>
 .container {
@@ -43,7 +37,6 @@
   background: rgba(255, 245, 0, 0.6);
   box-shadow: 0 0 20px rgb(235, 236, 238);
   display: flex;
-  justify-content: center;
   align-items: center;
   flex-direction: column;
   justify-content: flex-start;
@@ -61,10 +54,10 @@
 .tableContainer {
   box-sizing: border-box;
   width: 100%;
-  padding: 0 0.5%;
 }
 table {
   width: 100%;
+  text-align: left;
 }
 thead {
   width: 100%;
@@ -76,7 +69,7 @@ tbody {
 tr {
   width: 100%;
   display: grid;
-  grid-template-columns: 33% 33% 33%;
+  grid-template-columns: 50% 50%;
   font-size: 1.5em;
   margin-top: 1vh;
   margin-bottom: 1vh;
